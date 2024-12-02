2ddd2e2810d01539c0bb29452b9cf1537bd32a12
mynameisphil <phillip.rosner@outlook.com>
2024-12-02 20:02:34 +0100

    smoothbrained before-after fix to catch some strays

--- Python/day2/day2.py
+++ Python/day2/day2.py
@@ -13,28 +13,53 @@ def file_reader(file_path):
         contents = file.readlines()
     return contents
 
-inputList = file_reader(os.path.join(PATH, 'day2_test.txt'))
+inputList = file_reader(os.path.join(PATH, 'day2.txt'))
 
-SAFE_REPORT_COUNTER = 0
+PART1_SAFE_REPORT_COUNTER = 0
+PART2_SAFE_REPORT_COUNTER = 0
 
-badReportDict = {}
+def check_report(level_list):
+    """Checks the report if it is safe - returns a negative value if unresolveable"""
+    inc_count = 0
+    dec_count = 0
+    for y, current_value in enumerate(level_list):
+        if y+1 < len(level_list):
+            delta_value = int(level_list[y+1]) - int(current_value)
+            if -1 >= delta_value >= -3 :
+                dec_count += 1
+            elif 1 <= delta_value <= 3:
+                inc_count += 1
+            elif delta_value == 0:
+                return (False, y)
+            else:
+                return (False, y+1)
+            if dec_count >= 1 and inc_count >= 1:
+                return (False, y)
+        else:
+            if (dec_count > 0 and inc_count == 0) or (inc_count > 0 and dec_count == 0):
+                return (True,-1)
+    return (False, -1)
 
 for rawReport in inputList:
     reportList = rawReport.strip('\n').split(' ')
-    INCREMENT_COUNTER = 0
-    DECREMENT_COUNTER = 0
-    for i, currentValue in enumerate(reportList):
-        if i+1 < len(reportList):
-            deltaValue = int(reportList[i+1]) - int(currentValue)
-            if -1 >= deltaValue >= -3 :
-                DECREMENT_COUNTER += 1
-            elif 1 <= deltaValue <= 3:
-                INCREMENT_COUNTER += 1
-            else:
-                badReportDict[rawReport.strip('\n')] = i # not all bad values are added somehow ?
-                break
-        else:
-            if (DECREMENT_COUNTER > 0 and INCREMENT_COUNTER == 0) or (INCREMENT_COUNTER > 0 and DECREMENT_COUNTER == 0):
-                SAFE_REPORT_COUNTER += 1
+    resultTuple = check_report(reportList)
+    #Do Part 1
+    if resultTuple[0] is True:
+        PART1_SAFE_REPORT_COUNTER += 1
+    elif resultTuple[1] >= 0:
+        popIndex = resultTuple[1]
+        reportListAfter = reportList[:]
+        reportListBefore = reportList[:]
+        reportList.pop(popIndex)
+        reportListBefore.pop(popIndex-1)
+        if check_report(reportList)[0] is True:
+            PART2_SAFE_REPORT_COUNTER += 1
+        elif check_report(reportListBefore)[0] is True:
+            PART2_SAFE_REPORT_COUNTER += 1
+        elif popIndex < len(reportListAfter):
+            reportListAfter.pop(popIndex+1)
+            if check_report(reportListAfter)[0] is True:
+                PART2_SAFE_REPORT_COUNTER += 1
 
-print("Part1: ",SAFE_REPORT_COUNTER)
+print("Part1: ",PART1_SAFE_REPORT_COUNTER)
+print("Part2: ",PART1_SAFE_REPORT_COUNTER+PART2_SAFE_REPORT_COUNTER)
