01d95608cf5ba223d2e1eb8bcfe203e9a96f0788
mynameisphil <phillip.rosner@outlook.com>
2024-12-05 09:16:10 +0100

    moved some stuff around for easier console navigation

---
+++ Python/3/day3.py
@@ -0,0 +1,8 @@
+"""
+https://adventofcode.com/2024/day/3
+Day 3: Mull It Over
+"""
+import inspect
+import os
+PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# Using readlines() https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/
